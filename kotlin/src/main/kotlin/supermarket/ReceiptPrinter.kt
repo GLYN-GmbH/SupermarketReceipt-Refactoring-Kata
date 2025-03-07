2be0f5b6625407ecdbb42e1e29ad800c556d335a
Schwerdt_Svenja <svenja.schwerdt@glyn.de>
2025-03-07 16:12:51 +0100

    Teamday Finish 07.03.2025

--- kotlin/src/main/kotlin/supermarket/ReceiptPrinter.kt
+++ kotlin/src/main/kotlin/supermarket/ReceiptPrinter.kt
@@ -8,4 +8,5 @@ class ReceiptPrinter @JvmOverloads constructor(private val columnWidth: Int = 40
     fun printReceipt(receipt: Receipt): String = receipt.print(columnWidth)
 }
 
+
 fun Double.toUKFormat() = String.format(Locale.UK, "%.2f", this)
